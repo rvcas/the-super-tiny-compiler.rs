1ce58451c9c5c5ac30100977bb62f112c1ffa79f
rvcas <x@rvcas.dev>
2020-08-15 15:00:06 -0400

    chore(*): back to 4 spaces

--- src/main.rs
+++ src/main.rs
@@ -3,135 +3,135 @@ use std::io::{stdin, stdout, Write};
 
 #[derive(Debug)]
 enum Token {
-  Paren(char),
-  Number(String),
-  TString(String),
-  Name(String),
+    Paren(char),
+    Number(String),
+    TString(String),
+    Name(String),
 }
 
 fn tokenizer(input: &str) -> Vec<Token> {
-  let mut tokens: Vec<Token> = Vec::new();
+    let mut tokens: Vec<Token> = Vec::new();
 
-  let mut chars = input.chars().peekable();
+    let mut chars = input.chars().peekable();
 
-  while let Some(ch) = chars.next() {
-    match ch {
-      ' ' => {}
-      '(' | ')' => tokens.push(Token::Paren(ch)),
-      '0'..='9' => {
-        let mut value = String::new();
-
-        value.push(ch);
-
-        while let Some(sub) = chars.peek() {
-          match sub {
+    while let Some(ch) = chars.next() {
+        match ch {
+            ' ' => {}
+            '(' | ')' => tokens.push(Token::Paren(ch)),
             '0'..='9' => {
-              if let Some(next) = chars.next() {
-                value.push(next);
-              } else {
-                unreachable!()
-              }
+                let mut value = String::new();
+
+                value.push(ch);
+
+                while let Some(sub) = chars.peek() {
+                    match sub {
+                        '0'..='9' => {
+                            if let Some(next) = chars.next() {
+                                value.push(next);
+                            } else {
+                                unreachable!()
+                            }
+                        }
+                        _ => break,
+                    }
+                }
+
+                tokens.push(Token::Number(value));
             }
-            _ => break,
-          }
-        }
-
-        tokens.push(Token::Number(value));
-      }
-      '"' => {
-        let mut value = String::new();
-
-        while let Some(sub) = chars.peek() {
-          match sub {
             '"' => {
-              chars.next();
-              break;
+                let mut value = String::new();
+
+                while let Some(sub) = chars.peek() {
+                    match sub {
+                        '"' => {
+                            chars.next();
+                            break;
+                        }
+                        _ => {
+                            if let Some(next) = chars.next() {
+                                value.push(next);
+                            } else {
+                                unreachable!()
+                            }
+                        }
+                    }
+                }
+
+                tokens.push(Token::TString(value));
+            }
+            'a'..='z' => {
+                let mut value = String::new();
+
+                value.push(ch);
+
+                while let Some(sub) = chars.peek() {
+                    match sub {
+                        'a'..='z' => {
+                            if let Some(next) = chars.next() {
+                                value.push(next);
+                            } else {
+                                unreachable!()
+                            }
+                        }
+                        _ => break,
+                    }
+                }
+
+                tokens.push(Token::Name(value))
             }
             _ => {
-              if let Some(next) = chars.next() {
-                value.push(next);
-              } else {
-                unreachable!()
-              }
+                println!("I dont know what this character is: {}", ch);
+
+                break;
             }
-          }
         }
-
-        tokens.push(Token::TString(value));
-      }
-      'a'..='z' => {
-        let mut value = String::new();
-
-        value.push(ch);
-
-        while let Some(sub) = chars.peek() {
-          match sub {
-            'a'..='z' => {
-              if let Some(next) = chars.next() {
-                value.push(next);
-              } else {
-                unreachable!()
-              }
-            }
-            _ => break,
-          }
-        }
-
-        tokens.push(Token::Name(value))
-      }
-      _ => {
-        println!("I dont know what this character is: {}", ch);
-
-        break;
-      }
     }
-  }
 
-  return tokens;
+    return tokens;
 }
 
 fn main() {
-  let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
 
-  match args.get(1) {
-    None => println!("Error: expected atleast one argument"),
-    Some(arg) => {
-      if arg == "repl" {
-        let mut s = String::new();
+    match args.get(1) {
+        None => println!("Error: expected atleast one argument"),
+        Some(arg) => {
+            if arg == "repl" {
+                let mut s = String::new();
 
-        loop {
-          print!("\u{001b}[31;1mtiny> \u{001b}[36m");
+                loop {
+                    print!("\u{001b}[31;1mtiny> \u{001b}[36m");
 
-          let _ = stdout().flush();
+                    let _ = stdout().flush();
 
-          stdin()
-            .read_line(&mut s)
-            .expect("Did not enter a correct string");
+                    stdin()
+                        .read_line(&mut s)
+                        .expect("Did not enter a correct string");
 
-          if let Some('\n') = s.chars().next_back() {
-            s.pop();
-          }
+                    if let Some('\n') = s.chars().next_back() {
+                        s.pop();
+                    }
 
-          if let Some('\r') = s.chars().next_back() {
-            s.pop();
-          }
+                    if let Some('\r') = s.chars().next_back() {
+                        s.pop();
+                    }
 
-          if s == ":q" || s == ":quit" {
-            s.clear();
-            break;
-          }
+                    if s == ":q" || s == ":quit" {
+                        s.clear();
+                        break;
+                    }
 
-          let tokens = tokenizer(s.as_str());
+                    let tokens = tokenizer(s.as_str());
 
-          println!("\u{001b}[37;1mTokens: \u{001b}[35;1m{:?}", tokens);
+                    println!("\u{001b}[37;1mTokens: \u{001b}[35;1m{:?}", tokens);
 
-          s.clear();
+                    s.clear();
+                }
+            } else {
+                let tokens = tokenizer(arg.as_str());
+
+                println!("\u{001b}[37;1mTokens: \u{001b}[35;1m{:?}", tokens);
+            }
         }
-      } else {
-        let tokens = tokenizer(arg.as_str());
-
-        println!("\u{001b}[37;1mTokens: \u{001b}[35;1m{:?}", tokens);
-      }
     }
-  }
 }
